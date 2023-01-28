a7e1b160ee04a926ebb98e2768371b48c0e6c4d0
Nikita Lendiak <lendiak93@gmail.com>
2023-01-28 22:43:18 +0000

    fix send button, add instagram social button

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -4,7 +4,14 @@ import "./Socials.css";
 const Socials = (props) => {
   const buttons = [
     {
-      iconName: "fa-brands fa-linkedin-in",
+      iconName: "fa-brands fa-facebook",
+      iconSize: "1.6rem",
+      iconColor: "#3B6B98",
+      backgroundColor: "#111429",
+      link: "https://facebook.com",
+    },
+    {
+      iconName: "fa-brands fa-linkedin",
       iconSize: "1.35rem",
       iconColor: "#0e76a8",
       backgroundColor: "#151836",
@@ -18,24 +25,24 @@ const Socials = (props) => {
       link: "https://twitter.com",
     },
     {
-      iconName: "fa-brands fa-facebook",
+      iconName: "fa-brands fa-instagram",
       iconSize: "1.6rem",
-      iconColor: "#3B6B98",
-      backgroundColor: "#111429",
-      link: "https://facebook.com",
+      iconColor: "#DC2A7B",
+      backgroundColor: "#2B0E25",
+      link: "https://instagram.com",
     },
     {
       iconName: "fa-brands fa-github",
       iconSize: "1.45rem",
-      iconColor: "#171515",
-      backgroundColor: "#F5F5F5",
+      iconColor: "#F5F5F5",
+      backgroundColor: "#171515",
       link: "https://github.com",
     },
   ];
 
   return (
     <div className="socials">
-      <h2>My Socials</h2>
+      <h2>Social Media</h2>
       <ul className="socials-list">
         {buttons.map((button, index) => (
           <li key={"social-item" + index}>
