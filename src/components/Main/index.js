1f51278544c22c3b8d404476148d8a2c36e1c2ca
Nikita Lendiak <lendiak93@gmail.com>
2023-01-29 01:11:47 +0000

    add profile image

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -5,23 +5,32 @@ const Main = () => {
     <main className="body-container">
       <div className="Main-body">
         <section className="section-round-bg">
-          <h3>
-            Hello 👋
-            <br /> I'm Nikita Lendiak, <br />
-            <span className="text-accent">Full Stack Developer</span>.
-          </h3>
-          <p>
-            Over 5 years of experience in creating and maintaining websites for
-            a variety of clients. I specialize in using modern web technologies
-            such as HTML, CSS, JavaScript and PHP to create responsive and
-            user-friendly websites.
-          </p>
-          <p>
-            I am always looking for new challenges and opportunities to expand
-            my skills. Please take a look at my portfolio for examples of my
-            work and feel free to contact me for more information or to discuss
-            a project.
-          </p>
+          <div className="about-me">
+            <article>
+              <h3>
+                Hello 👋
+                <br /> I'm Nikita Lendiak, <br />
+                <span className="text-accent">Full Stack Developer</span>.
+              </h3>
+              <p>
+                Over 5 years of experience in creating and maintaining websites
+                for a variety of clients. I specialize in using modern web
+                technologies such as HTML, CSS, JavaScript and PHP to create
+                responsive and user-friendly websites.
+              </p>
+              <p>
+                I am always looking for new challenges and opportunities to
+                expand my skills. Please take a look at my portfolio for
+                examples of my work and feel free to contact me for more
+                information or to discuss a project.
+              </p>
+            </article>
+            <img
+              className="profile-image"
+              src="/assets/images/profile.jpg"
+              alt="Nikita's profile"
+            ></img>
+          </div>
 
           <div className="download-cv">
             <a className="btn" href="#top">
@@ -112,7 +121,7 @@ const Main = () => {
         <section>
           <h2>Portfolio</h2>
           <ul className="portfolio-list">
-          <li className="portfolio-item">
+            <li className="portfolio-item">
               <div className="image-wrapper">
                 <img
                   src="https://i.postimg.cc/3RDDYGtm/image-1.png"
@@ -190,6 +199,69 @@ const Main = () => {
                     This is basic online shop based on NodeJS, MongoDB This is
                     basic online shop based on NodeJS, MongoDB This is basic
                     online shop based on NodeJS, MongoDB
+                  </p>
+                </div>
+
+                <div class="bottom-flex">
+                  <div className="badge">
+                    <i
+                      class="fa-brands fa-square-js"
+                      style={{ color: "#F7DF1E" }}
+                    ></i>{" "}
+                    JavaScript
+                  </div>
+                  <div className="badge">
+                    <i
+                      class="fa-brands fa-html5"
+                      style={{ color: "#f06529" }}
+                    ></i>
+                    <i
+                      class="fa-brands fa-css3-alt"
+                      style={{ color: "#298EF1" }}
+                    ></i>{" "}
+                    HTML/CSS
+                  </div>
+                  <div className="badge">
+                    <i
+                      class="fa-brands fa-node-js"
+                      style={{ color: "#F7DF1E" }}
+                    ></i>{" "}
+                    NodeJS
+                  </div>
+                  <div className="badge">
+                    <i
+                      class="fa-solid fa-database"
+                      style={{ color: "#4DB33D" }}
+                    ></i>{" "}
+                    MongoDB
+                  </div>
+
+                  <div className="portfolio-actions">
+                    <button className="btn btn-regular-font">Live Demo</button>
+                    <button className="btn btn-round btn-with-icon btn-regular-font">
+                      <i
+                        class="fa-brands fa-github btn-icon"
+                        style={{ color: "black" }}
+                      ></i>
+                      GitHub
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className="portfolio-item">
+              <div className="image-wrapper">
+                <img
+                  src="https://i.postimg.cc/3RDDYGtm/image-1.png"
+                  alt="weather app"
+                ></img>
+              </div>
+
+              <div className="portfolio-details">
+                <div className="project-details">
+                  <h3>Online Shop</h3>
+                  <p className="app-info">
+                    This is basic online shop based on NodeJS
                   </p>
                 </div>
 
