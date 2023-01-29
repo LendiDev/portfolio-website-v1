2184eac2a0735ca89c18df29ee611a77e81ef28b
Nikita Lendiak <lendiak93@gmail.com>
2023-01-29 01:54:31 +0000

    add show/hide animation to a header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,49 @@
 import "./Header.css";
 import { Link } from "react-scroll";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import useRemToPixels from "../../hooks/useRemToPixels";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { useCallback, useEffect, useState } from "react";
 
 const Header = () => {
+  const [showHeaderBackground, setShowHeaderBackground] = useState("");
+
   const { width } = useWindowDimensions();
   const scrollToContactMeOffsetRem = width < 768 ? 4 : 6;
+  const headerHeight = useRemToPixels(4);
+
+  const handleScroll = useCallback(() => {
+    const topOffset = window.pageYOffset;
+    if (topOffset > headerHeight) {
+      if (showHeaderBackground !== "show-background") {
+        setShowHeaderBackground("show-background");
+      }
+    } else {
+      if (showHeaderBackground === "show-background") {
+        setShowHeaderBackground("hide-background");
+      }
+    }
+  }, [headerHeight, showHeaderBackground]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   const logoClickHandler = () => {
-   useNavigate.navigate('/');
-  }
+    useNavigate.navigate("/");
+  };
 
   return (
-    <header>
+    <header className={showHeaderBackground}>
       <div id="header-inner-container">
         <div className="header-logo">
-          <Link className="logo-link" target="_self" onClick={logoClickHandler} href="/">
+          <Link
+            className="logo-link"
+            target="_self"
+            onClick={logoClickHandler}
+            href="/"
+          >
             <h1>
               Lendi<span className="text-accent">.</span>Dev
             </h1>
